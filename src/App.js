3b48af87e32540266ae32bc2bad71bf0f34510f0
xaviguasch <xaviguasch@gmail.com>
2019-05-02 15:42:33 +0200

    added pickColor logic to Box

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      <h1>Color Boxes app</h1>
       <Box />
     </div>
   )
--- src/Box.js
+++ src/Box.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
+import { choice } from './helpers.js'
 import './Box.css'
 
 class Box extends Component {
+  static defaultProps = {
+    allColors: ['purple', 'magenta', 'violet', 'pink']
+  }
+
   constructor(props) {
     super(props)
-    this.state = { color: 'purple' }
+    this.state = { color: choice(this.props.allColors) }
 
     this.handleClick = this.handleClick.bind(this)
   }
 
+  pickColor() {
+    let newColor
+
+    do {
+      newColor = choice(this.props.allColors)
+    } while (newColor === this.state.color)
+
+    this.setState({ color: newColor })
+  }
+
   handleClick() {
-    console.log('clicked!!!!')
+    this.pickColor()
   }
 
   render() {
     return (
       <div className='Box' style={{ backgroundColor: this.state.color }} onClick={this.handleClick}>
-        <h1>Box Component</h1>
+        lala
       </div>
     )
   }
